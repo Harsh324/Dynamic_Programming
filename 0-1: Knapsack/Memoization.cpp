152a06645688f28172b8c746b81fa6ca6b6253d2
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 <harshravi324@gmail.com>
2022-06-25 08:38:21 +0530

    Soem changes done

---
+++ 0-1: Knapsack/Memoization.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int static DP[100][1000];
+
+auto x = memset(DP, -1, sizeof(DP));
+
+int Knapsack(int Wt[], int Val[], int Cap , int Sz)
+{
+    if(Sz == 0 || Cap == 0)
+        return 0;
+    
+    if(DP[Sz][Cap] == -1)
+        return DP[Sz][Cap];
+
+    if(Wt[Sz - 1] <= Cap)
+        return DP[Sz][Cap] = max( Val[Sz - 1] + Knapsack(Wt, Val, Cap - Wt[Sz - 1], Sz - 1), Knapsack(Wt, Val, Cap, Sz - 1));
+    
+    else if(Wt[Sz - 1] > Cap)
+        return DP[Sz][Cap] = Knapsack(Wt, Val, Cap, Sz - 1);
+}
+
+int main()
+{
+    int value[] = {20, 5, 10, 40, 15, 25};
+    int weight[] = {1, 2, 3, 8, 7, 4};
+    int W = 10;
+    cout<<Knapsack(weight, value, 10, 6)<<endl;
+}
