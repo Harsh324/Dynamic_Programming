0bb7202ca17a3cf340a3397faa57c77eb3167706
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 <harshravi324@gmail.com>
2022-06-26 16:38:22 +0530

    Recursive implementation of o-1 Knapsack added

--- 0-1:Knapsack/Recursive.cpp
+++ 0-1:Knapsack/Recursive.cpp
@@ -1,3 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int Knapsack(int Wt[], int Val[], int Cap, int Sz)
+{
+    if(Cap == 0 || Sz == 0)
+        return 0;
+
+    if(Wt[Sz - 1] <= Cap)
+        return max(Val[Sz - 1] + Knapsack(Wt, Val, Cap - Wt[Sz - 1], Sz - 1) , Knapsack(Wt, Val, Cap, Sz - 1));
+
+    else if(Wt[Sz - 1] > Cap)
+        return Knapsack(Wt, Val, Cap, Sz - 1);
+}
+
+
+
+int main()
+{
+    //memset(DP, -1, sizeof(DP));
+    int value[] = {20, 5, 10, 40, 15, 25};
+    int weight[] = {1, 2, 3, 8, 7, 4};
+    int W = 10;
+    cout<<Knapsack(weight, value, 10, 6)<<endl;
+    return 0;
+}
