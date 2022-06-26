d31a09483153d33d9809287953ac582868ea0e38
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 <harshravi324@gmail.com>
2022-06-26 22:34:41 +0530

    Memoization approach added for Subset sum problem

--- 0-1:Knapsack/Variations/Subset_sum_Problem/Memoization.cpp
+++ 0-1:Knapsack/Variations/Subset_sum_Problem/Memoization.cpp
@@ -5,28 +5,17 @@ bool DP[100][100];
 
 bool Sub_sum(int Arr[], int Sum, int N)
 {
-    for(int i = 0; i <= N; i++)
-    {
-        for(int j = 0; j <= Sum; j++)
-        {
-            if(i == 0)
-                DP[i][j] = false;
-            if(j == 0)
-                DP[i][j] = true;
-        }
-    }
+    if(Sum == 0)
+        return true;
+    
+    if(N == 0)
+        return false;
 
-    for(int i = 1; i <= N; i++)
-    {
-        for(int j = 1; j <= Sum; j++)
-        {
-            if(DP[i - 1][j] <= j)
-                DP[i][j] = DP[i - 1][j - DP[i][j]] || DP[i - 1][j];
-            else
-                DP[i][j] = DP[i - 1][j];
-        }
-    }
-    return DP[N][Sum];
+
+    if(DP[N - 1][Sum] <= Sum)
+        return DP[N][Sum] = DP[N - 1][Sum - DP[N][Sum]] || DP[N - 1][Sum];
+    else
+        DP[N][Sum] = DP[N - 1][Sum];
 }
 
 int main()
