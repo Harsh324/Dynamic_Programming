9479faa8c516291d79c8888cab825cb822de508d
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 <harshravi324@gmail.com>
2022-06-26 23:20:09 +0530

    Some changes done

--- 0-1:Knapsack/Recursive.cpp
+++ 0-1:Knapsack/Recursive.cpp
@@ -10,7 +10,7 @@ int Knapsack(int Wt[], int Val[], int Cap, int Sz)
     if(Wt[Sz - 1] <= Cap)
         return max(Val[Sz - 1] + Knapsack(Wt, Val, Cap - Wt[Sz - 1], Sz - 1) , Knapsack(Wt, Val, Cap, Sz - 1));
 
-    else if(Wt[Sz - 1] > Cap)
+    else
         return Knapsack(Wt, Val, Cap, Sz - 1);
 }
 
@@ -19,9 +19,9 @@ int Knapsack(int Wt[], int Val[], int Cap, int Sz)
 int main()
 {
     //memset(DP, -1, sizeof(DP));
-    int value[] = {20, 5, 10, 40, 15, 25};
-    int weight[] = {1, 2, 3, 8, 7, 4};
-    int W = 10;
-    cout<<Knapsack(weight, value, 10, 6)<<endl;
+    int value[] = {60, 100, 120};
+    int weight[] = {10, 20, 30};
+    int W = 50;
+    cout<<Knapsack(weight, value, W, 3)<<endl;
     return 0;
 }
