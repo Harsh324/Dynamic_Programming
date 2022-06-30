d2538e8d0dd3b37060048418382396d20edf6e44
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 <harshravi324@gmail.com>
2022-06-30 10:41:49 +0530

    Recursive implementation added

--- 0-1:Knapsack/Variations/Equal_sum_Partition/Recursive.cpp
+++ 0-1:Knapsack/Variations/Equal_sum_Partition/Recursive.cpp
@@ -1,8 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool Sub_sum(int Arr[], int Sum, int N)
+{
+    if(Sum == 0)
+        return true;
+    
+    if(N == 0)
+        return false;
+
+    if(Arr[N - 1] <= Sum)
+        return (Sub_sum(Arr, Sum - Arr[N - 1], N - 1) || Sub_sum(Arr, Sum , N - 1));
+
+    else
+        return Sub_sum(Arr, Sum, N - 1);
+}
+
+
+bool Equal_Sum_Partiton(int Arr[], int N)
+{
+    int Sum = 0;
+    for(int i = 0; i < N; i++)
+        Sum += Arr[i];
+
+    if(Sum % 2 != 0)
+        return false;
+    else
+        return Sub_sum(Arr, Sum / 2, N);
+}
 
 int main()
 {
-    return 0;
+    int set[] = {3, 34, 4, 12, 5, 2};
+    int sum = 30;
+    cout<<Sub_sum(set, sum, 6)<<endl;
 }
