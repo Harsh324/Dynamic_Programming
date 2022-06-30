7e03fa80addacbb6bb9cbf0954926601c5cb2b8d
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 <harshravi324@gmail.com>
2022-06-30 10:46:31 +0530

    Memoized implementation added

---
+++ 0-1:Knapsack/Variations/Equal_sum_Partition/Memoization.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool DP[100][100];
+
+bool Sub_sum(int Arr[], int Sum, int N)
+{
+    for(int i = 0; i <= N; i++)
+    {
+        for(int j = 0; j <= Sum; j++)
+        {
+            if(i == 0)
+                DP[i][j] = false;
+            if(j == 0)
+                DP[i][j] = true;
+        }
+    }
+
+    for(int i = 1; i <= N; i++)
+    {
+        for(int j = 1; j <= Sum; j++)
+        {
+            if(Arr[i - 1] <= j)
+                DP[i][j] = DP[i  -1][j - Arr[i - 1]] || DP[i - 1][j];
+            else
+                DP[i][j] = DP[i - 1][j];
+        }
+    }
+    return DP[N][Sum];
+}
+
+bool Equal_Sum_Partiton(int Arr[], int N)
+{
+    int Sum = 0;
+    for(int i = 0; i < N; i++)
+        Sum += Arr[i];
+
+    if(Sum % 2 != 0)
+        return false;
+    else
+        return Sub_sum(Arr, Sum / 2, N);
+}
+
+
+int main()
+{
+    int set[] = {1, 3, 3, 3};
+    cout<<Equal_Sum_Partiton(set, 4)<<endl;
+}
+
