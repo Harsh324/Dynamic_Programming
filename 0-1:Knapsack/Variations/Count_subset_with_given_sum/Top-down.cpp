75160a90b7a1c6914b2b86c9729414971aa00f89
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 <harshravi324@gmail.com>
2022-07-01 08:07:29 +0530

    Top Donwn implementation file added

--- 0-1:Knapsack/Variations/Count_subset_with_given_sum/Top-down.cpp
+++ 0-1:Knapsack/Variations/Count_subset_with_given_sum/Top-down.cpp
@@ -3,12 +3,37 @@ using namespace std;
 
 int DP[100][100];
 
-int Count_subset(int Arr[], int Sum, int N)
+int Count_Subset(int Arr[], int Sum, int N)
 {
-    
+    for(int i = 0; i <= N; i++)
+    {
+        for(int j = 0; j <= Sum; j++)
+        {
+            if(i == 0)
+                DP[i][j] = 0;
+            if(j == 0)
+                DP[i][j] = 1;
+        }
+    }
+
+    for(int i = 1; i <= N; i++)
+    {
+        for(int j = 1; j <= Sum; j++)
+        {
+            if(Arr[i - 1] <= Sum)
+            {
+                DP[i][j] = DP[i - 1][Sum - Arr[i - 1]] + DP[i - 1][Sum];
+            }
+            else
+                DP[i][j] = DP[i - 1][Sum];
+        }
+    }
+    return DP[N][Sum];
 }
 
 int main()
 {
-    return 0;
+    int Arr[] = {2, 3, 5, 6, 8 ,10};
+    int Sum = 10;
+    cout<<Count_Subset(Arr, Sum, 6)<<endl;
 }
